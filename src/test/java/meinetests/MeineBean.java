247b0254304beecd3fb3a9b676f198046929da64
heckenmann <benedikt.stoffel@t-online.de>
2016-03-12 22:45:03 +0100

    init

---
+++ src/test/java/meinetests/MeineBean.java
@@ -0,0 +1,14 @@
+package meinetests;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+@Stateless
+@LocalBean
+public class MeineBean {
+
+	public String sagHallo(final String name) {
+		return "Hallo " + name;
+	}
+
+}
---
+++ src/test/java/meinetests/Test1.java
@@ -0,0 +1,28 @@
+package meinetests;
+
+import javax.inject.Inject;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.testng.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class Test1 extends Arquillian {
+
+	@Deployment
+	public static JavaArchive getArchive() {
+		return ShrinkWrap.create(JavaArchive.class, "test.jar").addClasses(Test1.class, MeineBean.class).addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+	}
+	
+	@Inject
+	private MeineBean meineBean;
+	
+	@Test
+	public void testHello() {
+		Assert.assertNotNull(meineBean);
+	}
+}
